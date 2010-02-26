a7668a18c5076a652225047aab54f0c5ce6f0fcc
Zef Houssney <zef@madebykiwi.com>
2010-02-26 01:02:49 -0700

    Added custom options for checkboxes, radios, and selects. Separate options for initial page load.

--- jquery.form_toggle.js
+++ jquery.form_toggle.js
@@ -2,79 +2,86 @@ jQuery(function($) {
 
   $.fn.form_toggle = function(options) {
     var settings = $.extend({
-      prefix : 'toggle',
-      reverse : false,
-      speed : 'normal'
-      // command : function(area) {
-      //   area.toggle('normal');
-      // }
+      prefix        : 'toggle',
+      reverse       : false,
+      checkbox      : 'normal',
+      radio         : null,
+      select        : null,
+      checkbox_init : null,
+      radio_init    : null,
+      select_init   : null
+      
     }, options);
     var functions = {
-      show_or_hide_element : function(selected, element, animate) {
+      show_or_hide_element : function(selected, element, args) {
         if ((selected && !settings.reverse) || (settings.reverse && !selected)) {
-          element.show(settings.speed);
+          element.show(args);
         } else{
-          if (animate == false) {
-            element.hide();
-          } else {
-            element.hide(settings.speed);
-          }
+          element.hide(args);
         }
       },
-      handle_checked_state : function(controller, element, animate) {
+      handle_checked_state : function(controller, element, args) {
         selected = $(controller).attr('checked');
-        functions.show_or_hide_element(selected, element, animate);
+        functions.show_or_hide_element(selected, element, args);
       },
-      handle_select : function(select, animate) {
+      handle_select : function(select, args) {
         $(select).find('option').each(function(index) {
           var element = $('.' + $(this).val());
           var selected = $(this).val() == $(select).val();
 
-          functions.show_or_hide_element(selected, element, animate);
+          functions.show_or_hide_element(selected, element, args);
         });
       }
       // settings.command(element);
     };
-    var elements = $('[class|=' + settings.prefix + ']');
+    var elements = $('[class*=' + settings.prefix + ']');
 
 
     elements.filter(':checkbox').each(function(index) {
-      var element;
+      checkbox = $(this);
+      matcher = new RegExp('^' + settings.prefix + '-');
 
-      element = $($(this).attr('class').split(' ')[0].replace(settings.prefix + '-', '.'));
-      functions.handle_checked_state(this, element, false);
-
-      $(this).change(function() {
-        functions.handle_checked_state(this, element);
+      classes = $.grep(checkbox.attr('class').split(' '), function(class_name, index) {
+        return class_name.match(matcher);
       });
+
+      $.each(classes, function(index) {
+
+        var element = $(this.replace(settings.prefix + '-', '.'));
+        functions.handle_checked_state(checkbox, element, settings.checkbox_init);
+
+        checkbox.change(function() {
+          functions.handle_checked_state(this, element, settings.checkbox);
+        });
+      });
+      
     });
 
     elements.filter(':radio').each(function(index) {
       var element;
 
       element = $('.' + $(this).val());
-      functions.handle_checked_state(this, element, false);
+      functions.handle_checked_state(this, element, settings.radio_init);
 
       $(this).change(function() {
         var group = $(this).attr('name');
 
         $(':radio[name = ' + group + ']').each(function(index) {
           element = $('.' + $(this).val());
-          functions.handle_checked_state(this, element);
+          functions.handle_checked_state(this, element, settings.radio);
         });
 
       });
     });
 
     elements.filter('select').each(function(index) {
-      functions.handle_select(this, false);
-      
+      functions.handle_select(this, settings.select_init);
+
       $(this).change(function() {
-        functions.handle_select(this, false);
+        functions.handle_select(this, settings.select);
       });
 
     });
-
   };
 
 });
